f1c3f2519f13ab60563d32a93bb084b73f5319a1
Priyajit Das <49169518+Priyajit100@users.noreply.github.com>
2024-05-07 17:01:34 +0000

    commit new

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,7 +3,7 @@
 // string 
 // boolean=> true/false  
 // null=> standalone value 
-// undefined=> 
+// undefined=> value not assigned
 // symbol=>unique
 console.log(typeof null);
 console.log(typeof undefined);
