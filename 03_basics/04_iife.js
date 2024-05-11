c1da06a4465d8974d1bf078f02d28b5461fb0ad5
Priyajit Das <49169518+Priyajit100@users.noreply.github.com>
2024-05-11 16:09:30 +0000

    commit

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,11 +1,14 @@
 // Immediately Invoked Function Expressions (IIFE)
 
 
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
-})();
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
 
-( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('rocky')
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('rocky')
+let x=5
+let y= x>=5 ? "Yes":"No"
+console.log(y)
