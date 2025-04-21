3c9e6fdaa67e98270a4e234e40cf9200e08fdefa
Priyajit Das <49169518+Priyajit100@users.noreply.github.com>
2025-04-21 16:54:47 +0000

    changes

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -12,7 +12,8 @@ console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
-console.log(undefined == 0);
+console.log(undefined == NaN);
+console.log(undefined == null);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
