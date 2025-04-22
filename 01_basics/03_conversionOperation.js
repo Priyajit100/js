929105fe0124e3e8873a3e652b06eb392f2d414a
Priyajit Das <49169518+Priyajit100@users.noreply.github.com>
2025-04-22 16:19:17 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -62,9 +62,13 @@ let str3 = str1 + str2
 
  //console.log( (3 + 4) * 5 % 3);
 
- console.log(+true);
- console.log(+"");
-
+//  console.log(+true);
+//  console.log(+"");
+//  console.log(+null);     
+//  console.log(+undefined); 
+//  console.log(+"123");    
+//  console.log(+{});      
+//  console.log(+[]);      
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
