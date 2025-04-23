d608fd6ba78d40a032b7bd839124dec505458cc2
Priyajit Das <49169518+Priyajit100@users.noreply.github.com>
2025-04-23 16:28:39 +0000

    changes

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,35 +1,43 @@
 const name = "hitesh"
 const repoCount = 50
+//console.log(repoCount.valueOf());
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
-
+//console.log(typeof(gameName));
 // console.log(gameName[0]);
 //console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+//console.log(gameName.charAt(5));
+//console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+//console.log(newString) ;
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+const anotherString = gameName.slice(-8, 4) // no output as start>end
+// console.log(anotherString);
 
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+ // console.log(newStringOne);
+//console.log(newStringOne.trim());
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('xy'))
+//console.log(url.includes('xy'))
 
-console.log(gameName.split('-'));
+//console.log(gameName.split('-'));
+const x=[1,2,2,2,4];
+// const newLocal = x.splice(1, 3);
+// console.log(x);
+// x.splice(3,1,4);
+//  console.log(x);
+// x.splice(3, 0, 4);
+// console.log(x);
