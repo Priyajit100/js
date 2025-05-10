ee2b234778de8289d6ddc62772df6c7e81f63834
Priyajit Das <49169518+Priyajit100@users.noreply.github.com>
2025-05-10 18:29:40 +0000

    changes

--- 07_Project/six.js
+++ 07_Project/six.js
@@ -8,7 +8,8 @@ const randomColor = function () {
     }
     return color;
   };
-  
+  // const randomColor = () => `rgb(${[0,0,0].map(() => Math.floor(Math.random() * 256)).join(',')})`;
+
   let intervalId;
   const startChangingColor = function () {
     if (!intervalId) {
@@ -27,5 +28,23 @@ const randomColor = function () {
   document.querySelector('#start').addEventListener('click', startChangingColor);
   
   document.querySelector('#stop').addEventListener('click', stopChangingColor);
+  const generatedColors = new Set(); // To track previously generated colors
+
+// const randomColor = function () {
+//   const hex = '0123456789ABCDEF';
+//   let color;
+  
+//   do {
+//     color = '#';
+//     for (let i = 0; i < 6; i++) {
+//       color += hex[Math.floor(Math.random() * 16)];
+//     }
+//   } while (generatedColors.has(color)); // Keep generating until unique
+
+//   generatedColors.add(color); // Add the newly generated color to the set
+//   return color;
+// };
+
+// console.log(randomColor()); // Will return a unique color
   
   
